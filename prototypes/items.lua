1c87d314bac57c44156d50691928a6690ed11323
Tom <robot256@GMAIL.COM>
2020-02-02 08:26:11 -0500

    Reverted 0.18 changes so this can be released for 0.17

--- prototypes/items.lua
+++ prototypes/items.lua
@@ -3,7 +3,7 @@ data:extend({
     type = "item",
     name = "train-stop-dispatcher",
     icon = "__Dispatcher__/graphics/icons/dispatcher.png",
-    icon_size = 32, icon_mipmaps = 1,
+    icon_size = 32,
     subgroup = "transport",
     order = "a[train-system]-c[train-stop]-a",
     place_result = "train-stop-dispatcher",
